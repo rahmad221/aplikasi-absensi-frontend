6b21ab5b4c885a85f8dd46fe738281354201c032
rahmad221 <alhidayattu@gmail.com>
2025-10-06 15:50:12 +0700

    api

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dimensiv2.abatagroup.co.id/",
+  baseURL: "https://api.cyberdeveloper.my.id/",
   // baseURL: "http://127.0.0.1:8000/",
   headers: {
     "Content-Type": "application/json",
